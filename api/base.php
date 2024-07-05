7d9a7069eefaa3f427aeed0f091f9b7d380b7bb3
jing <jinng.vip@gmail.com>
2024-07-05 13:45:33 +0800

    計數器

--- api/base.php
+++ api/base.php
@@ -134,3 +134,17 @@ function dd($array)
 
 
 $User = new DB("users");
+$Total = new DB("total");
+$News = new DB('news');
+
+
+if (!isset($_SESSION['total'])) {
+    if ($Total->count(['date' => date("Y-m-d")]) > 0) {
+        $total = $Total->find(['date' => date("Y-m-d")]);
+        $total['total']++;
+        $Total->save($total);
+    } else {
+        $Total->save(['date' => date("Y-m-d"), 'total' => 1]);
+    }
+    $_SESSION['total'] = $Total->find(['date' => date("Y-m-d")])['total'];
+}
--- index.php
+++ index.php
@@ -19,11 +19,11 @@
 	<iframe name="back" style="display:none;"></iframe>
 	<div id="all">
 		<div id="title">
-			00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36
+			<?= date("m 月 d 號 l"); ?> | 今日瀏覽: <?= $_SESSION['total']; ?> | 累積瀏覽: <?= q("select sum(`total`) as 'total' from `total`")[0]['total']; ?>
 			<a href="index.php" style="float:right">回首頁</a>
 		</div>
 		<div id="title2">
-			<a href="index.php">
+			<a href="index.php" title="健康促進網-回首頁">
 				<img src="icon/02B01.jpg" alt="">
 			</a>
 		</div>
